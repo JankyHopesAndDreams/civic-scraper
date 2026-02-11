30f5f05aadea66f065569ecb0350f7422e613e87
jankyhopesanddreams <jankyhopesanddreams@proton.me>
2026-02-11 11:38:37 -0800

    added municodeMeetingsSite to platform init

--- civic_scraper/platforms/__init__.py
+++ civic_scraper/platforms/__init__.py
@@ -1,5 +1,6 @@
 from .civic_clerk.site import CivicClerkSite
 from .civic_plus.site import Site as CivicPlusSite
+from .municodemeetings.site import Site as MunicodeMeetingsSite
 from .granicus.site import GranicusSite
 from .legistar.site import Site as LegistarSite
 from .primegov.site import PrimeGovSite
